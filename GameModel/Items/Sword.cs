70bf5cf5ffa9d3b358d561dc52cd5749a3f2802f
anatolii-dotsenko <a.dotsenko.dev@gmail.com>
2025-12-02 20:58:07 +0200

    adding Items/sword.cs and Items/shield.cs

---
+++ GameModel/Items/Sword.cs
@@ -0,0 +1,15 @@
+using GameModel.Abilities;
+
+namespace GameModel.Items
+{
+    /// <summary>
+    /// Basic weapon increasing attack power.
+    /// </summary>
+    public class Sword : Item
+    {
+        public Sword()
+            : base(name: "Iron Sword", attack: 5)
+        {
+        }
+    }
+}
