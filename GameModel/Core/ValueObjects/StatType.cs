32bb12fdca2f9068a65fc5edf9c8c92a53a7a79b
anatolii-dotsenko <a.dotsenko.dev@gmail.com>
2025-12-08 00:51:32 +0200

    new stats to support the formulas

--- GameModel/Core/ValueObjects/StatType.cs
+++ GameModel/Core/ValueObjects/StatType.cs
@@ -4,9 +4,12 @@ namespace GameModel.Core.ValueObjects
     {
         Health,
         MaxHealth,
-        Attack,
-        Armor,
-        Mana,    // Example of extensibility
-        Speed    // Example of extensibility
+        Attack,      // Physical Attack
+        MagicPower,  // Magic Attack
+        Armor,       // Flat Physical Defense
+        Resistance,  // % Physical Mitigation (0-100)
+        MagicResist, // % Magic Mitigation (0-100)
+        Penetration, // % Resistance Negation
+        Speed        // For turn order
     }
 }
