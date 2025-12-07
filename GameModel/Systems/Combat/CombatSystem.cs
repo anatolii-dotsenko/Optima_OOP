7413a1cefc7e220f153daec62ca12e1a1c96cad3
anatolii-dotsenko <a.dotsenko.dev@gmail.com>
2025-12-07 17:57:25 +0200

    observer implementation

--- GameModel/Systems/Combat/CombatSystem.cs
+++ GameModel/Systems/Combat/CombatSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using GameModel.Core.Contracts;
 using GameModel.Core.Entities;
 using GameModel.Core.ValueObjects;
@@ -6,14 +7,14 @@ namespace GameModel.Systems.Combat
 {
     public class CombatSystem : ICombatSystem
     {
-        private readonly ICombatLogger _logger;
+        // Observer: Events for subscribers
+        public event Action<AttackResult>? OnAttackPerformed;
+        public event Action<AbilityResult>? OnAbilityUsed;
+        public event Action<HealResult>? OnHealed;
 
-        public CombatSystem(ICombatLogger logger)
-        {
-            _logger = logger;
-        }
+        // Note: Removed direct ICombatLogger dependency to decouple logic from IO.
 
-        public AttackResult Attack(ICombatEntity attacker, ICombatEntity defender)
+        public void Attack(ICombatEntity attacker, ICombatEntity defender)
         {
             var attStats = attacker.GetStats();
             var defStats = defender.GetStats();
@@ -25,29 +26,33 @@ namespace GameModel.Systems.Combat
             defender.TakeDamage(damage);
 
             var result = new AttackResult(attacker.Name, defender.Name, damage);
-            _logger.LogAttack(result);
-            return result;
+            
+            // Notify Observers
+            OnAttackPerformed?.Invoke(result);
         }
 
-        public AbilityResult UseAbility(ICombatEntity user, ICombatEntity target, Ability ability)
+        public void UseAbility(ICombatEntity user, ICombatEntity target, Ability ability)
         {
             var userStats = user.GetStats();
             var targetStats = target.GetStats();
 
-            int damage = ability.CalculateDamage(userStats, targetStats);
+            // Ability uses Template Method internally
+            int damage = ability.Apply(userStats, targetStats);
             target.TakeDamage(damage);
 
             var result = new AbilityResult(user.Name, target.Name, ability.Name, damage);
-            _logger.LogAbility(result);
-            return result;
+            
+            // Notify Observers
+            OnAbilityUsed?.Invoke(result);
         }
 
-        public HealResult Heal(ICombatEntity healer, int amount)
+        public void Heal(ICombatEntity healer, int amount)
         {
             healer.Heal(amount);
             var result = new HealResult(healer.Name, amount);
-            _logger.LogHeal(result);
-            return result;
+            
+            // Notify Observers
+            OnHealed?.Invoke(result);
         }
     }
 }
